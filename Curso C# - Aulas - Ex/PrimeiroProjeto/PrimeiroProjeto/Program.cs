9250b19919fa4e266a4229b73df6c7ba0a9b0fb3
Ragner Alves <ragnerf@hotmail.com>
2023-05-04 14:49:19 -0300

    exercicios

---
+++ Curso C# - Aulas - Ex/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string frase = Console.ReadLine();
+string x = Console.ReadLine();
+string y = Console.ReadLine();
+string z = Console.ReadLine();
+
+string[] v = Console.ReadLine().Split(' ');
+string a = v[0];
+string b = v[1];
+string c = v[2];
+
+Console.WriteLine("Voce digitou: " );
+Console.WriteLine(frase);
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(z);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+Console.ReadKey();
