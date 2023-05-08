2a7af6a6c359143163feda32ec87795ba8df6ab2
Ragner Alves <ragnerf@hotmail.com>
2023-05-08 10:36:05 -0300

    update exercicio

--- Curso C# - Aulas - Ex/PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ Curso C# - Aulas - Ex/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -65,17 +65,82 @@
 //Console.WriteLine(idade);
 //Console.WriteLine(altura);
 
-Console.WriteLine("Digite um numero inteiro para sabermos se é par ou impar: ");
-int num = int.Parse(Console.ReadLine());
+//exercicio 01
+//Console.WriteLine("Digite um numero inteiro para sabermos se é par ou impar: ");
+//int num = int.Parse(Console.ReadLine());
 
-if (num % 2 == 0)
+//if (num % 2 == 0)
+//{
+//    Console.WriteLine("Número digitado é par");
+//} 
+//else
+//{
+//    Console.WriteLine("Número digitado é impar.");
+//}
+
+//exercicio02
+//Console.WriteLine("Digite dois valores inteiros para sabermos se são multiplos entre si: ");
+
+//string[] vet = Console.ReadLine().Split(' ');
+//int num1 = int.Parse(vet[0]);
+//int num2 = int.Parse(vet[1]);
+
+//if (num1 % num2 == 0 || num2 % num1 == 0)
+//{ 
+//    Console.WriteLine("Os número digitados são multiplos entre si");
+//}
+//else
+//{
+//    Console.WriteLine("Os números digitados não são multiplos entre si.");
+//}
+
+//exercicio03
+
+using System.Globalization;
+
+internal class Program
 {
-    Console.WriteLine("Número digitado é par");
-} 
-else
-{
-    Console.WriteLine("Número digitado é impar.");
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Favor digite o codigo do item e a quantidade abaixo para o valor total da compra: ");
+
+        string[] vet = Console.ReadLine().Split(' ');
+        int cod1 = int.Parse(vet[0]);
+        int quantidade = int.Parse(vet[1]);
+
+        double total = 0;
+        if (cod1 == 1)
+        {
+            total = quantidade * 4.0;
+        }
+        else if (cod1 == 2)
+        {
+            total = quantidade * 4.5;
+        }
+        else if (cod1 == 3)
+        {
+            total = quantidade * 5.0;
+        }
+        else if (cod1 == 4)
+        {
+            total = quantidade * 2.0;
+        }
+        else if (cod1 == 5)
+        {
+            total = quantidade * 1.50;
+        }
+        
+        Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        Console.ReadKey();
+    }
 }
 
 
-Console.ReadKey();
+
+//string[] vet = Console.ReadLine().Split(' ');
+//int cod1 = int.Parse(vet[0]);
+//int quantidade = int.Parse(vet[1]);
+
+//double total;
