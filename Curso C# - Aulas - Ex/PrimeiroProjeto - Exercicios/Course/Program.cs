bc551b9fbd1acf17ed5634417accdc8a31ac31fa
Ragner Alves <ragnerf@hotmail.com>
2023-08-18 17:02:44 -0300

    exercicios - update

--- Curso C# - Aulas - Ex/PrimeiroProjeto - Exercicios/Course/Program.cs
+++ Curso C# - Aulas - Ex/PrimeiroProjeto - Exercicios/Course/Program.cs
@@ -524,37 +524,7 @@ namespace Course
             //    }
             //}
 
-            //Triangulo x, y;
-            //x = new Triangulo();
-            //y = new Triangulo();
-
-            //Console.WriteLine("Digite as medidas do triangulo X: ");
-            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            //Console.WriteLine("Digite as medidas do triangulo Y: ");
-            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            //double p = (x.A + x.B + x.C) / 2.0;
-            //double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-            //p = (y.A + y.B + y.C) / 2.0;
-            //double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C)); ;
-
-            //Console.WriteLine("Valor da área de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            //Console.WriteLine("Valor da área de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            //if (areaX > areaY)
-            //{
-            //    Console.WriteLine("A maior area é a área X");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("A maior área é a área Y");
-            //}
+            //Exercicios:
 
             //Pessoa p1, p2;
             //p1 = new Pessoa();
@@ -593,6 +563,36 @@ namespace Course
             //double mediaSalario = (f1.Salario + f2.Salario) / 2;
 
             //Console.WriteLine("Salario medio é de: " + mediaSalario.ToString("F4", CultureInfo.InvariantCulture));
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite as medidas do triangulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite as medidas do triangulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            
+            double areaY = y.Area();
+
+            Console.WriteLine("Valor da área de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor da área de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("A maior area é a área X");
+            }
+            else
+            {
+                Console.WriteLine("A maior área é a área Y");
+            }
 
             Console.ReadKey();
         }
