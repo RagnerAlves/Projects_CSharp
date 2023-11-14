36a1ef7f8966f426c82f7681fb1f5825f580d515
Ragner Alves <ragnerf@hotmail.com>
2023-11-14 14:51:28 -0300

    web_api - create_entity_model

---
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaID { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
