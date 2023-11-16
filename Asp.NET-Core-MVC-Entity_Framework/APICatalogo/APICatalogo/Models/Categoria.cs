1b096b41e22c110962c45cc1da4a5bc82c95ceda
Ragner Alves <ragnerf@hotmail.com>
2023-11-16 18:53:21 -0300

    update_schema_database

--- Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Models/Categoria.cs
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categoria")]
 public class Categoria
 {   
     public Categoria()
     {
         Produtos = new Collection<Produto>();
     }
-    
 
+    [Key]
     public int CategoriaID { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Produto> Produtos { get; set; }
--- Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/obj/Debug/net7.0/APICatalogo.AssemblyInfo.cs
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/obj/Debug/net7.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5520545b68afcd8e8df5dcabe09e87a102b12ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+244bf24cd45c8107a32c5efcb48735ff9fcb5bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
