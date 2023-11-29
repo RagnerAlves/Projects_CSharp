be2ce0c8e96429b99725c732511c5c4b1202be07
Ragner Alves <ragnerf@hotmail.com>
2023-11-29 15:32:23 -0300

    produto_controller_post

--- Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -27,5 +27,30 @@ namespace APICatalogo.Controllers
 
             return produto;
         }
+
+        [HttpGet("id:int", Name="ObterProduto")]
+
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto) 
+        {
+            if (produto is null)
+                return BadRequest();
+
+                _context.Produtos?.Add(produto);
+                _context.SaveChanges();
+
+                return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
     }
 }
