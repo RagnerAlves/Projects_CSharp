9bd1b7738a73865f8e8be1dce1762e22d4583f15
Ragner Alves <ragnerf@hotmail.com>
2023-11-29 14:36:26 -0300

    create_controller_all_produtos

---
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,31 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produto = _context.Produtos?.ToList();
+            if(produto is null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+    }
+}
