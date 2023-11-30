41883be8d626dc3df7f77e13d77aeb9bbb198707
Ragner Alves <ragnerf@hotmail.com>
2023-11-30 09:21:19 -0300

    updade_nome_repository

--- Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/obj/Debug/net7.0/APICatalogo.AssemblyInfo.cs
+++ Asp.NET-Core-MVC-Entity_Framework/APICatalogo/APICatalogo/obj/Debug/net7.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd1b7738a73865f8e8be1dce1762e22d4583f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
