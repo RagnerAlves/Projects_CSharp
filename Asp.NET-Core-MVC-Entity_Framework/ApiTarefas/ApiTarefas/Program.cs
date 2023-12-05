0666a43210a16946992a0588d1c1d2edb64b91f3
Ragner Alves <ragnerf@hotmail.com>
2023-12-05 16:01:58 -0300

    minimal_api

---
+++ Asp.NET-Core-MVC-Entity_Framework/ApiTarefas/ApiTarefas/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+opt.UseInMemoryDatabase("TarefasDB"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Olá mundo");
+
+app.MapGet("frases", async () =>
+await new HttpClient().GetStringAsync("https://ron-swanson-wuotes.herokuapp.com/v2/quotes"));
+
+
+
+app.Run();
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
