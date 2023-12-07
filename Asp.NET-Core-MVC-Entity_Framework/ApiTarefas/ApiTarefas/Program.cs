e47aa3e73f0df190d5eecce2f87fa7e5bc5d978c
Ragner Alves <ragnerf@hotmail.com>
2023-12-07 14:27:42 -0300

    update__end__points

--- Asp.NET-Core-MVC-Entity_Framework/ApiTarefas/ApiTarefas/Program.cs
+++ Asp.NET-Core-MVC-Entity_Framework/ApiTarefas/ApiTarefas/Program.cs
@@ -39,6 +39,31 @@ app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
 });
 
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+
+    if (tarefa is null) return Results.NotFound();
+
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluida = inputTarefa.IsConcluida;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if(await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+
+    return Results.NotFound();
+});
+
 app.Run();
 
 class Tarefa
