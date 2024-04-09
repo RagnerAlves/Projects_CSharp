84e5f337b83181f29f472bd54a73b52b7daca83f
Ragner Alves <ragnerf@hotmail.com>
2024-04-09 15:33:02 -0300

    update

--- Asp.NET-Core-MVC-Entity_Framework/ApiTarefas/ApiTarefas/obj/Debug/net6.0/ApiTarefas.AssemblyInfo.cs
+++ Asp.NET-Core-MVC-Entity_Framework/ApiTarefas/ApiTarefas/obj/Debug/net6.0/ApiTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b84953959f5785267d59d7c7ae28cec2db093cdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
